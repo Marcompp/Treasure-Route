34495459bacca9757a449cd4529078d8025a67e5
juliano nassar <juliano nassar>
2017-06-01 17:28:14 -0300

    s

--- Classes.py
+++ Classes.py
@@ -11,7 +11,7 @@ def Mar(barco,Mar_img,contador,screen,largura,altura,Sol):
             screen.blit(Mar_img,(0,h))
             h+=  30
         screen.blit(barco,(largura/8,altura/8+30))
-        screen.blit(Sol,(largura-250,50))
+        
        
         
     else:
@@ -19,7 +19,7 @@ def Mar(barco,Mar_img,contador,screen,largura,altura,Sol):
             screen.blit(Mar_img,(-40,h))
             h+=  30
         screen.blit(barco,(largura/8,altura/8+40))
-        screen.blit(Sol,(largura-250,60))
+        
         if contador == 60:
             contador = 0
     return contador
@@ -27,9 +27,17 @@ def Mar(barco,Mar_img,contador,screen,largura,altura,Sol):
 def sair():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.mixer.quit()
                 pygame.quit()
                 sys.exit()
                 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            Som("dano.ogg")
+        if keys[pygame.K_s]:
+            Mudar_musica("musicapiratas.ogg")
+        
+        
 def Nuvem_spawn(nuvem,Lista_Nuvem,largura,ymax,conta,screen,velocidade,tamanho_min,tamanho_max,chance):
         
             
@@ -39,7 +47,7 @@ def Nuvem_spawn(nuvem,Lista_Nuvem,largura,ymax,conta,screen,velocidade,tamanho_m
     
         if Nuvem_True<50:
             tamanho = randint(tamanho_min,tamanho_max)
-            y = randint(-40,ymax)
+            y = randint(-10,ymax)
             x = largura
         
             coisa = pygame.transform.scale(nuvem,(tamanho,tamanho))
@@ -60,3 +68,14 @@ def Nuvem_spawn(nuvem,Lista_Nuvem,largura,ymax,conta,screen,velocidade,tamanho_m
             else:    
                 conta += 1
                 screen.blit(Nuvem["imagem"],(Nuvem["x"],Nuvem["y"]))
+                
+def Mudar_musica(Nova_musica):
+    pygame.mixer.quit()
+    pygame.mixer.init()
+    musica = pygame.mixer.music.load(Nova_musica) 
+    pygame.mixer.music.play(-1)              
+                
+def Som(File_Name):
+    som = pygame.mixer.Sound(File_Name)
+    pygame.mixer.Sound.play(som)
+    
