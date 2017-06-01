947034444f47b1195d0bcc16a95191b9fc60e36f
marco moliterno <marco moliterno>
2017-05-31 21:27:28 -0300

    fix

--- Start.py
+++ Start.py
@@ -7,6 +7,7 @@ import random
 import Batalha
 import Misc
 import json
+import Menu
 
 
 randevs=["Newspaper","TrainSkl","Castaway","Magicbo","Rowboat"]
@@ -42,57 +43,79 @@ paper = pygame.image.load('paper2.png').convert()
 pygame.display.set_caption('Pyga')
 clock = pygame.time.Clock()
 
-
-phase = 0
-timeod = 0
-progress = 0
-nexev = 0
 while True:
-	ship = Misc.Loadship()#{"surface":1,"pos":35,"speed":3,"durability":10}
-	party = Misc.Loadparty()
-	supply = Misc.Loadsupply()
-	pos = Misc.Loadpos()
-	story = Misc.Loadstory()
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			exit()
-	screen.blit(background, (0, 0))
-	#screen.blit(indicator,(ship["pos"],10))
-	screen.blit(goal,(465,15))
-	#screen.blit(linha, 20,20)
-	screen.blit(indicator,(pos,10))
-
-	Batalha.blitcards(screen)
-	Batalha.blitsupply(screen)
-
-	if progress == 20:
-		pos += ship["speed"]
-		progress = 0
-	if pos == 460:
-		getattr(Evento,Goal)(screen)
-
-	if timeod == 90:
-		if supply["food"] >= len(party)*2:
-			getattr(Evento,foodevs[random.randrange(len(foodevs))])(screen)
-		elif supply["food"] != 0:
-			getattr(Evento,ratievs[random.randrange(len(ratievs))])(screen)
-		else:
-			getattr(Evento,hungevs[random.randrange(len(hungevs))])(screen)
+	Evento.endar()
+	select = Menu.mainmenu(screen)
+	if select == 2:
+		Menu.newgame(screen)
+		select = 1
+	if select == 1:
+		try:
+			ship = Misc.Loadship()#{"surface":1,"pos":35,"speed":3,"durability":10}
+			party = Misc.Loadparty()
+			supply = Misc.Loadsupply()
+			pos = Misc.Loadpos()
+			story = Misc.Loadstory()
+		except ValueError:
+			newgame()
+		phase = 0
 		timeod = 0
-	if nexev >=23:
-		x = random.randrange(1,9)
-		if x >= 5:
-			y = random.randrange(len(party))
-			if party[y]["mor"]<= 0:
-				getattr(Evento,despevs[random.randrange(len(despevs))])(y ,screen)
-			else:
-				getattr(Evento,randevs[random.randrange(len(randevs))])(screen)
-			nexev =0
-	progress+= 1
-	nexev += 1
-	timeod +=1
-	Misc.Savepos(pos)
-	
-	#Misc.Save(party,supply,ship,pos,story)
-	pygame.display.update()
-	time_passed = clock.tick(30)
+		progress = 0
+		nexev = 0
+		while True:
+			ship = Misc.Loadship()#{"surface":1,"pos":35,"speed":3,"durability":10}
+			party = Misc.Loadparty()
+			supply = Misc.Loadsupply()
+			pos = Misc.Loadpos()
+			story = Misc.Loadstory()
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					exit()
+			screen.blit(background, (0, 0))
+			#screen.blit(indicator,(ship["pos"],10))
+			screen.blit(goal,(465,15))
+			#screen.blit(linha, 20,20)
+			screen.blit(indicator,(pos,10))
+
+			Batalha.blitcards(screen)
+			Batalha.blitsupply(screen)
+
+			if progress == 20:
+				pos += ship["speed"]
+				progress = 0
+			if pos == 460:
+				getattr(Evento,Goal)(screen)
+
+			if timeod == 90:
+				if supply["food"] >= len(party)*2:
+					getattr(Evento,foodevs[random.randrange(len(foodevs))])(screen)
+				elif supply["food"] != 0:
+					getattr(Evento,ratievs[random.randrange(len(ratievs))])(screen)
+				else:
+					getattr(Evento,hungevs[random.randrange(len(hungevs))])(screen)
+				timeod = 0
+			if nexev >=23:
+				x = random.randrange(1,9)
+				if x >= 5:
+					y = random.randrange(len(party))
+					if party[y]["mor"]<= 0:
+						getattr(Evento,despevs[random.randrange(len(despevs))])(y ,screen)
+					else:
+						getattr(Evento,randevs[random.randrange(len(randevs))])(screen)
+					nexev =0
+			progress+= 1
+			nexev += 1
+			timeod +=1
+			Misc.Savepos(pos)
+			
+			#Misc.Save(party,supply,ship,pos,story)
+			pygame.display.update()
+			time_passed = clock.tick(30)
+	if select ==3:
+		Text=["Programming, writing and design - Marco",
+			"Programing and graphics design - Juliano",
+			"Sound design - Leonardo",
+			"",
+			"Special Thanks: Luciano, Camila"]
+		Evento.walloftext(Text,screen)
+		Evento.proceed(screen)
