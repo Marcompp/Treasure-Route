8482f527821c7830a8e2a7df99d296e4f2a9a22e
marco moliterno <marco moliterno>
2017-06-01 17:30:53 -0300

    fix

--- Classes.py
+++ Classes.py
@@ -4,23 +4,23 @@ from random import randint
 
 def Mar(barco,Mar_img,contador,screen,largura,altura,Sol):
     h = -40
-    contador+=1
+    contador+=3.5
     if contador<30:
         
         while h != 200:
             screen.blit(Mar_img,(0,h))
             h+=  30
-        screen.blit(barco,(largura/8,altura/8+30))
-        screen.blit(Sol,(largura-250,50))
+        screen.blit(barco,(largura/8-100,altura/8-40))
+        
        
         
     else:
         while h != 200:
             screen.blit(Mar_img,(-40,h))
             h+=  30
-        screen.blit(barco,(largura/8,altura/8+40))
-        screen.blit(Sol,(largura-250,60))
-        if contador == 60:
+        screen.blit(barco,(largura/8-100,altura/8-30))
+        
+        if contador >= 60:
             contador = 0
     return contador
 
@@ -50,13 +50,14 @@ def Nuvem_spawn(nuvem,Lista_Nuvem,largura,ymax,conta,screen,velocidade,tamanho_m
                  "tamanho":tamanho}
             
             Lista_Nuvem.append(NUVEM)
-        for Nuvem in Lista_Nuvem:
-            Nuvem["x"] -=velocidade
+        while conta <= len(Lista_Nuvem)-1:
+            Lista_Nuvem[conta]["x"] -=velocidade
         
-            if Nuvem["x"] <= -Nuvem["tamanho"]:
+            if Lista_Nuvem[conta]["x"] <= -Lista_Nuvem[conta]["tamanho"]:
             
-                Lista_Nuvem.remove(Nuvem)
-                conta -=1
-            else:    
-                conta += 1
-                screen.blit(Nuvem["imagem"],(Nuvem["x"],Nuvem["y"]))
+                Lista_Nuvem.remove(Lista_Nuvem[conta])
+                
+
+            else:   
+                screen.blit(Lista_Nuvem[conta]["imagem"],(Lista_Nuvem[conta]["x"],Lista_Nuvem[conta]["y"]))
+                conta +=1
